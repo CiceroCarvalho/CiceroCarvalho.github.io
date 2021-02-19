b9517105078f089de6927490e1bfb37ec09ecd72
Cicero Carvalho de Jesus <72409915+CiceroCarvalho@users.noreply.github.com>
2021-02-18 22:01:30 -0300

    Add files via upload

--- jwnwt .js
+++ jwnwt .js
@@ -412,23 +412,3 @@ function esconder()
     document.getElementById("conteudo").style.display = "block";
 }
 
-document.addEventListener("DOMContentLoaded", function(){
-        
-	var checkbox = document.querySelectorAll("input[type='checkbox']");
- 
-	for(var item of checkbox){
-	   item.addEventListener("click", function(){
-		  localStorage.s_item ? // verifico se existe localStorage
-			 localStorage.s_item = localStorage.s_item.indexOf(this.id+",") == -1 // verifico de localStorage contém o id
-			 ? localStorage.s_item+this.id+"," // não existe. Adiciono a id no loaclStorage
-			 : localStorage.s_item.replace(this.id+",","") : // já existe, apago do localStorage
-		  localStorage.s_item = this.id+",";  // não existe. Crio com o id do checkbox
-	   });
-	}
- 
-	if(localStorage.s_item){ // verifico se existe localStorage
-	   for(var item of checkbox){ // existe, percorro as checkbox
-		  item.checked = localStorage.s_item.indexOf(item.id+",") != -1 ? true : false; // marco true nas ids que existem no localStorage
-	   }
-	}
- });
